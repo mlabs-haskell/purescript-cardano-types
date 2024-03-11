4cb12adad1212b909535ead6366277ffe15acb11
Vladimir Kalnitsky <vladimir@mlabs.city>
2024-03-11 23:08:47 +0400

    Add a Semigroup instance for Coin

--- src/Cardano/Types/Coin.purs
+++ src/Cardano/Types/Coin.purs
@@ -1,16 +1,18 @@
 module Cardano.Types.Coin where
 
-import Prelude
+import Prelude hiding (zero)
 
 import Aeson (class DecodeAeson, class EncodeAeson)
 import Cardano.Types.BigNum (BigNum(BigNum))
 import Cardano.Types.BigNum as BigNum
 import Data.Generic.Rep (class Generic)
 import Data.Lattice (class JoinSemilattice, class MeetSemilattice)
-import Data.Maybe (Maybe)
-import Data.Newtype (class Newtype)
+import Data.Maybe (Maybe, maybe)
+import Data.Newtype (class Newtype, wrap)
 import Data.Semiring as Num
 import Data.Show.Generic (genericShow)
+import Effect.Exception (throw)
+import Effect.Unsafe (unsafePerformEffect)
 import Safe.Coerce (coerce)
 import Test.QuickCheck.Arbitrary (class Arbitrary, arbitrary)
 import Test.QuickCheck.Gen (suchThat)
@@ -23,6 +25,14 @@ derive newtype instance Eq Coin
 derive newtype instance Ord Coin
 derive newtype instance DecodeAeson Coin
 derive newtype instance EncodeAeson Coin
+
+instance Semigroup Coin where
+  append (Coin x) (Coin y) =
+    unsafePerformEffect $ maybe (throw "Coin.append: numeric overflow") (pure <<< wrap)
+      $ BigNum.add x y
+
+instance Monoid Coin where
+  mempty = zero
 
 instance Arbitrary Coin where
   arbitrary = Coin <<< BigNum.fromInt <$> suchThat arbitrary (_ >= Num.zero)
