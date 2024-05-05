99bf9b100a6c2cacf944b940939da64def4779b1
Vladimir Kalnitsky <vladimir@mlabs.city>
2024-05-05 17:29:22 +0400

    Do not use plutus-based Aeson instances for AssetName

--- src/Cardano/Types/AssetName.purs
+++ src/Cardano/Types/AssetName.purs
@@ -6,11 +6,10 @@ import Aeson
   ( class DecodeAeson
   , class EncodeAeson
   , JsonDecodeError(TypeMismatch)
-  , caseAesonObject
+  , caseAesonString
   , encodeAeson
-  , getField
   )
-import Cardano.AsCbor (class AsCbor, encodeCbor)
+import Cardano.AsCbor (class AsCbor, decodeCbor, encodeCbor)
 import Cardano.FromData (class FromData, fromData)
 import Cardano.FromMetadata (class FromMetadata, fromMetadata)
 import Cardano.Serialization.Lib (assetName_name, assetName_new, fromBytes, toBytes)
@@ -18,14 +17,12 @@ import Cardano.Serialization.Lib as Csl
 import Cardano.ToData (class ToData, toData)
 import Cardano.ToMetadata (class ToMetadata, toMetadata)
 import Cardano.Types.Internal.Helpers (decodeUtf8)
-import Data.ByteArray (ByteArray, byteArrayToHex, byteLength, hexToByteArray)
-import Data.Either (Either(Left, Right), either, note)
+import Data.ByteArray (ByteArray, byteLength, hexToByteArray)
+import Data.Either (Either(Right, Left), either)
 import Data.Function (on)
 import Data.Generic.Rep (class Generic)
-import Data.Maybe (Maybe(Just, Nothing), fromJust)
+import Data.Maybe (Maybe(Just, Nothing), fromJust, maybe)
 import Data.Newtype (class Newtype, unwrap, wrap)
-import Data.String as String
-import Data.TextEncoder (encodeUtf8)
 import Partial.Unsafe (unsafePartial)
 import Test.QuickCheck (class Arbitrary, arbitrary)
 import Test.QuickCheck.Gen (resize)
@@ -72,32 +69,14 @@ fromAssetName arrayHandler stringHandler (AssetName assetNameCsl) = either
   where
   bs = assetName_name assetNameCsl
 
--- | Corresponds to the Haskell instance at https://github.com/input-output-hk/plutus/blob/4fd86930f1dc628a816adf5f5d854b3fec578312/plutus-ledger-api/src/Plutus/V1/Ledger/Value.hs#L155:
 instance DecodeAeson AssetName where
-  decodeAeson = caseAesonObject (Left $ TypeMismatch "Expected object") $
-    \aes -> do
-      tkstr <- getField aes "unAssetName"
-      case String.take 3 tkstr of
-        "\x0000000x" -> do -- this is 3 characters '\NUL' '0' 'x'
-          let stripped = String.drop 3 tkstr -- strip the \NUL followed by "0x"
-          ba <-
-            note
-              (TypeMismatch $ "Expected base16 encoded string got " <> stripped)
-              $ hexToByteArray stripped
-          pure $ AssetName $ assetName_new ba
-        "\x0\x0\x0" -> Right $ tkFromStr (String.drop 2 tkstr) -- if the original started with \NUL, we prepended 2 additional \NULs
-        _ -> Right $ tkFromStr tkstr
-    where
-    tkFromStr :: String -> AssetName
-    tkFromStr = AssetName <<< assetName_new <<< wrap <<< encodeUtf8
+  decodeAeson = do
+    maybe (Left $ TypeMismatch "Expected hex-encoded asset name") Right <<<
+      caseAesonString Nothing
+        (Just <=< decodeCbor <=< map wrap <<< hexToByteArray)
 
 instance EncodeAeson AssetName where
-  encodeAeson = encodeAeson <<< { "unAssetName": _ } <<< fromAssetName
-    (\ba -> "\x0" <> "0x" <> byteArrayToHex ba)
-    ( \s -> case String.take 1 s of
-        "\x0" -> "\x0\x0" <> s
-        _ -> s
-    )
+  encodeAeson sh = encodeAeson $ encodeCbor sh
 
 instance Show AssetName where
   show (AssetName tn) = "(mkAssetName " <> show (assetName_name tn) <> ")"
