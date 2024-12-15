0fb9b4596fa54af069ae8ee1feb94961657b095d
Marcus Fernandes <marcus@mlabs.city>
2024-12-15 19:07:08 -0300

    Handle `PlutusScript` language version in `TransactionWitnessSet`

--- src/Cardano/Types/TransactionWitnessSet.purs
+++ src/Cardano/Types/TransactionWitnessSet.purs
@@ -19,12 +19,16 @@ import Cardano.Data.Lite
   , transactionWitnessSet_nativeScripts
   , transactionWitnessSet_new
   , transactionWitnessSet_plutusData
-  , transactionWitnessSet_plutusScripts
+  , transactionWitnessSet_plutusScripts_v1
+  , transactionWitnessSet_plutusScripts_v2
+  , transactionWitnessSet_plutusScripts_v3
   , transactionWitnessSet_redeemers
   , transactionWitnessSet_setBootstraps
   , transactionWitnessSet_setNativeScripts
   , transactionWitnessSet_setPlutusData
-  , transactionWitnessSet_setPlutusScripts
+  , transactionWitnessSet_setPlutusScripts_v1
+  , transactionWitnessSet_setPlutusScripts_v2
+  , transactionWitnessSet_setPlutusScripts_v3
   , transactionWitnessSet_setRedeemers
   , transactionWitnessSet_setVkeys
   , transactionWitnessSet_vkeys
@@ -35,17 +39,19 @@ import Cardano.Types.BootstrapWitness (BootstrapWitness)
 import Cardano.Types.BootstrapWitness as BoostrapWitness
 import Cardano.Types.BootstrapWitness as BootstrapWitness
 import Cardano.Types.Internal.Helpers (withNonEmptyArray)
+import Cardano.Types.Language (Language(PlutusV1, PlutusV2, PlutusV3))
 import Cardano.Types.NativeScript (NativeScript)
 import Cardano.Types.NativeScript as NativeScript
 import Cardano.Types.PlutusData (PlutusData)
 import Cardano.Types.PlutusData as PlutusData
-import Cardano.Types.PlutusScript (PlutusScript)
+import Cardano.Types.PlutusScript (PlutusScript(PlutusScript))
 import Cardano.Types.PlutusScript as PlutusScript
 import Cardano.Types.Redeemer (Redeemer)
 import Cardano.Types.Redeemer as Redeemer
 import Cardano.Types.Vkeywitness (Vkeywitness)
 import Cardano.Types.Vkeywitness as Vkeywitness
 import Data.Array (nub)
+import Data.Array as Array
 import Data.Generic.Rep (class Generic)
 import Data.Lens (Lens')
 import Data.Lens.Iso.Newtype (_Newtype)
@@ -54,6 +60,7 @@ import Data.Maybe (Maybe, fromMaybe)
 import Data.Newtype (class Newtype, unwrap, wrap)
 import Data.Nullable (Nullable, toMaybe)
 import Data.Show.Generic (genericShow)
+import Data.Tuple.Nested ((/\))
 import Effect.Unsafe (unsafePerformEffect)
 import Type.Proxy (Proxy(Proxy))
 
@@ -95,14 +102,23 @@ fromCsl ws =
   where
   use :: forall a. (Csl.TransactionWitnessSet -> Nullable a) -> Maybe a
   use f = toMaybe (f ws)
+
+  setLanguage :: Language -> PlutusScript -> PlutusScript
+  setLanguage lang (PlutusScript (ba /\ _)) = PlutusScript (ba /\ lang)
+
   vkeys = map Vkeywitness.fromCsl $ fromMaybe []
     $ unpackListContainer <$> use transactionWitnessSet_vkeys
   nativeScripts = map NativeScript.fromCsl $ fromMaybe []
     $ unpackListContainer <$> use transactionWitnessSet_nativeScripts
   bootstraps = map BoostrapWitness.fromCsl $ fromMaybe []
     $ unpackListContainer <$> use transactionWitnessSet_bootstraps
-  plutusScripts = map PlutusScript.fromCsl $ fromMaybe []
-    $ unpackListContainer <$> use transactionWitnessSet_plutusScripts
+  plutusScripts_v1 = map (setLanguage PlutusV1 <<< PlutusScript.fromCsl) $ fromMaybe []
+    $ unpackListContainer <$> use transactionWitnessSet_plutusScripts_v1
+  plutusScripts_v2 = map (setLanguage PlutusV2 <<< PlutusScript.fromCsl) $ fromMaybe []
+    $ unpackListContainer <$> use transactionWitnessSet_plutusScripts_v2
+  plutusScripts_v3 = map ((setLanguage PlutusV3) <<< PlutusScript.fromCsl) $ fromMaybe []
+    $ unpackListContainer <$> use transactionWitnessSet_plutusScripts_v3
+  plutusScripts = plutusScripts_v1 <> plutusScripts_v2 <> plutusScripts_v3
   plutusData = map PlutusData.fromCsl $ fromMaybe []
     $ unpackListContainer <$> use transactionWitnessSet_plutusData
   redeemers = map Redeemer.fromCsl $ fromMaybe []
@@ -126,13 +142,19 @@ toCsl
     transactionWitnessSet_setNativeScripts ws
   withNonEmptyArray (BootstrapWitness.toCsl <$> bootstraps) $
     transactionWitnessSet_setBootstraps ws
-  withNonEmptyArray (PlutusScript.toCsl <$> nub plutusScripts) $
-    transactionWitnessSet_setPlutusScripts ws
+  withNonEmptyArray (PlutusScript.toCsl <$> nub ps_v1) $ transactionWitnessSet_setPlutusScripts_v1 ws
+  withNonEmptyArray (PlutusScript.toCsl <$> nub ps_v2) $ transactionWitnessSet_setPlutusScripts_v2 ws
+  withNonEmptyArray (PlutusScript.toCsl <$> nub ps_v3) $ transactionWitnessSet_setPlutusScripts_v3 ws
   withNonEmptyArray (PlutusData.toCsl <$> plutusData) $
     transactionWitnessSet_setPlutusData ws
   withNonEmptyArray (Redeemer.toCsl <$> redeemers) $
     transactionWitnessSet_setRedeemers ws
   pure ws
+  where
+  filterByLang lang = Array.filter (\(PlutusScript (_ /\ l)) -> lang == l)
+  ps_v1 = filterByLang PlutusV1 plutusScripts
+  ps_v2 = filterByLang PlutusV2 plutusScripts
+  ps_v3 = filterByLang PlutusV3 plutusScripts
 
 _redeemers :: Lens' TransactionWitnessSet (Array Redeemer)
 _redeemers = _Newtype <<< prop (Proxy :: Proxy "redeemers")
