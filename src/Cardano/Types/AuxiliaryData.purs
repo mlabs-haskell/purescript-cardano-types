6db5c2d6ced78c2235f51bd812966f0d7f7db36d
Marcus Fernandes <marcus@mlabs.city>
2024-12-17 09:22:07 -0300

    Handle `PlutusScript` language version in `AuxiliaryData`

--- src/Cardano/Types/AuxiliaryData.purs
+++ src/Cardano/Types/AuxiliaryData.purs
@@ -13,10 +13,14 @@ import Cardano.Data.Lite
   ( auxiliaryData_metadata
   , auxiliaryData_nativeScripts
   , auxiliaryData_new
-  , auxiliaryData_plutusScripts
+  , auxiliaryData_plutusScripts_v1
+  , auxiliaryData_plutusScripts_v2
+  , auxiliaryData_plutusScripts_v3
   , auxiliaryData_setMetadata
   , auxiliaryData_setNativeScripts
-  , auxiliaryData_setPlutusScripts
+  , auxiliaryData_setPlutusScripts_v1
+  , auxiliaryData_setPlutusScripts_v2
+  , auxiliaryData_setPlutusScripts_v3
   , packListContainer
   , unpackListContainer
   )
@@ -24,18 +28,22 @@ import Cardano.Data.Lite as Csl
 import Cardano.Types.AuxiliaryDataHash (AuxiliaryDataHash)
 import Cardano.Types.GeneralTransactionMetadata (GeneralTransactionMetadata)
 import Cardano.Types.GeneralTransactionMetadata as GeneralTransactionMetadatum
+import Cardano.Types.Internal.Helpers (withNonEmptyArray)
+import Cardano.Types.Language (Language(PlutusV1, PlutusV2, PlutusV3))
 import Cardano.Types.NativeScript (NativeScript)
 import Cardano.Types.NativeScript as NativeScript
-import Cardano.Types.PlutusScript (PlutusScript)
+import Cardano.Types.PlutusScript (PlutusScript(PlutusScript))
 import Cardano.Types.PlutusScript as PlutusScript
 import Control.Apply (lift2)
-import Data.Array (union)
+import Data.Array (nub, union)
+import Data.Array as Array
 import Data.Generic.Rep (class Generic)
-import Data.Maybe (Maybe(Nothing))
+import Data.Maybe (Maybe(Just, Nothing), fromMaybe)
 import Data.Newtype (class Newtype, unwrap, wrap)
-import Data.Nullable (toMaybe)
+import Data.Nullable (Nullable, toMaybe)
 import Data.Show.Generic (genericShow)
 import Data.Traversable (for_)
+import Data.Tuple.Nested ((/\))
 import Effect.Unsafe (unsafePerformEffect)
 
 newtype AuxiliaryData = AuxiliaryData
@@ -84,13 +92,39 @@ toCsl
     GeneralTransactionMetadatum.toCsl >>> auxiliaryData_setMetadata ad
   for_ nativeScripts $
     map NativeScript.toCsl >>> packListContainer >>> auxiliaryData_setNativeScripts ad
-  for_ plutusScripts $
-    map PlutusScript.toCsl >>> packListContainer >>> auxiliaryData_setPlutusScripts ad
+  withNonEmptyArray (PlutusScript.toCsl <$> nub ps_v1) $ auxiliaryData_setPlutusScripts_v1 ad
+  withNonEmptyArray (PlutusScript.toCsl <$> nub ps_v2) $ auxiliaryData_setPlutusScripts_v2 ad
+  withNonEmptyArray (PlutusScript.toCsl <$> nub ps_v3) $ auxiliaryData_setPlutusScripts_v3 ad
   pure ad
+
+  where
+  filterByLang lang = Array.filter (\(PlutusScript (_ /\ l)) -> lang == l)
+  ps_v1 = filterByLang PlutusV1 $ fromMaybe [] plutusScripts
+  ps_v2 = filterByLang PlutusV2 $ fromMaybe [] plutusScripts
+  ps_v3 = filterByLang PlutusV3 $ fromMaybe [] plutusScripts
 
 fromCsl :: Csl.AuxiliaryData -> AuxiliaryData
 fromCsl csl = AuxiliaryData
   { metadata: toMaybe (auxiliaryData_metadata csl) <#> GeneralTransactionMetadatum.fromCsl
-  , nativeScripts: toMaybe (auxiliaryData_nativeScripts csl) <#> unpackListContainer >>> map NativeScript.fromCsl
-  , plutusScripts: toMaybe (auxiliaryData_plutusScripts csl) <#> unpackListContainer >>> map PlutusScript.fromCsl
+  , nativeScripts: emptyToNothing $ toMaybe (auxiliaryData_nativeScripts csl) <#> unpackListContainer >>> map
+      NativeScript.fromCsl
+  , plutusScripts: if (Array.null plutusScripts) then Nothing else Just plutusScripts
   }
+  where
+
+  use :: forall a. (Csl.AuxiliaryData -> Nullable a) -> Maybe a
+  use f = toMaybe (f csl)
+
+  emptyToNothing (Just []) = Nothing
+  emptyToNothing x = x
+
+  setLanguage :: Language -> PlutusScript -> PlutusScript
+  setLanguage lang (PlutusScript (ba /\ _)) = PlutusScript (ba /\ lang)
+
+  plutusScripts_v1 = map (setLanguage PlutusV1 <<< PlutusScript.fromCsl) $ fromMaybe []
+    $ unpackListContainer <$> use auxiliaryData_plutusScripts_v1
+  plutusScripts_v2 = map (setLanguage PlutusV2 <<< PlutusScript.fromCsl) $ fromMaybe []
+    $ unpackListContainer <$> use auxiliaryData_plutusScripts_v2
+  plutusScripts_v3 = map ((setLanguage PlutusV3) <<< PlutusScript.fromCsl) $ fromMaybe []
+    $ unpackListContainer <$> use auxiliaryData_plutusScripts_v3
+  plutusScripts = plutusScripts_v1 <> plutusScripts_v2 <> plutusScripts_v3
