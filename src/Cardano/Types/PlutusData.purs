84071f5d09f8b2615947fb4dc5f31686d1b6422c
Marcus Fernandes <marcus@mlabs.city>
2024-12-18 15:03:21 -0300

    Update `PlutusData` CDL methods

--- src/Cardano/Types/PlutusData.purs
+++ src/Cardano/Types/PlutusData.purs
@@ -25,10 +25,12 @@ import Aeson
   )
 import Cardano.AsCbor (class AsCbor)
 import Cardano.Data.Lite
-  ( constrPlutusData_alternative
+  ( PlutusMapKeys
+  , PlutusMapValues
+  , constrPlutusData_alternative
   , constrPlutusData_data
   , constrPlutusData_new
-  , packMultiMapContainer
+  , packMapContainer
   , plutusData_asBytes
   , plutusData_asConstrPlutusData
   , plutusData_asInteger
@@ -39,7 +41,9 @@ import Cardano.Data.Lite
   , plutusData_newInteger
   , plutusData_newList
   , plutusData_newMap
-  , unpackMultiMapContainer
+  , plutusMap_keys
+  , plutusMap_values
+  , unpackMapContainer
   )
 import Cardano.Data.Lite as Csl
 import Cardano.Data.Lite.Internal (packListContainer, unpackListContainer)
@@ -47,21 +51,20 @@ import Cardano.Types.BigInt (fromCsl, toCsl) as BigInt
 import Cardano.Types.BigNum (BigNum)
 import Cardano.Types.BigNum as BigNum
 import Control.Alt ((<|>))
-import Data.Array (modifyAtIndices, singleton, snoc) as Array
+import Data.Array (modifyAtIndices, singleton, snoc, zip) as Array
 import Data.Array.NonEmpty as NA
 import Data.ByteArray (ByteArray, byteArrayToHex, hexToByteArray)
 import Data.Either (Either(Left))
 import Data.Generic.Rep (class Generic)
 import Data.List (List)
-import Data.List (fromFoldable, uncons) as List
+import Data.List (uncons) as List
 import Data.Log.Tag (TagSet, tag, tagSetTag)
 import Data.Log.Tag as TagSet
 import Data.Map (Map)
-import Data.Map (empty, insert, lookup) as Map
+import Data.Map (insert, lookup) as Map
 import Data.Maybe (Maybe(Just, Nothing), fromJust)
 import Data.Newtype (unwrap, wrap)
 import Data.Nullable (toMaybe)
-import Data.Profunctor.Strong ((***))
 import Data.Show.Generic (genericShow)
 import Data.Traversable (for)
 import Data.Tuple (Tuple(Tuple))
@@ -204,32 +207,7 @@ toCsl = case _ of
     plutusData_newList <<< packListContainer $ map toCsl pd
 
   convertPlutusMap :: Array (PlutusData /\ PlutusData) -> Csl.PlutusData
-  convertPlutusMap mp =
-    plutusData_newMap $ packMultiMapContainer
-      $ map (toCsl *** (packListContainer <<< map toCsl))
-      $ flip (groupValues Map.empty zero) mempty
-      $ List.fromFoldable mp
-    where
-    -- Group by key while preserving key order. Note that the serialization
-    -- roundtrip property is broken in presence of duplicate keys; however,
-    -- the order of values corresponding to each key is maintained.
-    groupValues
-      :: Map PlutusData Int
-      -> Int
-      -> List (PlutusData /\ PlutusData)
-      -> Array (PlutusData /\ Array PlutusData)
-      -> Array (PlutusData /\ Array PlutusData)
-    groupValues idxMap i l acc =
-      case List.uncons l of
-        Just { head: key /\ value, tail: xs } ->
-          case Map.lookup key idxMap of
-            Just idx ->
-              groupValues idxMap i xs $
-                Array.modifyAtIndices [ idx ] (map (flip Array.snoc value)) acc
-            Nothing ->
-              groupValues (Map.insert key i idxMap) (i + one) xs $
-                Array.snoc acc (key /\ Array.singleton value)
-        Nothing -> acc
+  convertPlutusMap mp = plutusData_newMap $ packMapContainer $ map (\(Tuple k v) -> Tuple (toCsl k) (toCsl v)) mp
 
   convertPlutusInteger :: BigInt -> Csl.PlutusData
   convertPlutusInteger =
@@ -253,11 +231,10 @@ fromCsl pd = unsafePartial $ fromJust $
 
   convertPlutusMap :: Csl.PlutusMap -> PlutusData
   convertPlutusMap pm =
-    Map $ unpackMultiMapContainer pm >>= \(k /\ vs) ->
-      let
-        cslK = fromCsl k
-      in
-        unpackListContainer vs >>= fromCsl >>> Tuple cslK >>> pure
+    Map $ map (\(Tuple k v) -> Tuple (fromCsl k) (fromCsl v)) $ Array.zip keys values
+    where
+    keys = unpackListContainer $ plutusMap_keys pm
+    values = unpackListContainer $ plutusMap_values pm
 
   convertPlutusList :: Csl.PlutusList -> PlutusData
   convertPlutusList = unpackListContainer >>> map fromCsl >>> List
